996f2c958b3f649bd8dfa1ee728b95fceb3ffd4f
AwaPreciousNkeng <awaprecious3t@gmail.com>
2026-02-16 00:55:08 +0100

    First commit

---
+++ src/main/java/com/codewithpcodes/epipredict/EpiPredictApplication.java
@@ -0,0 +1,13 @@
+package com.codewithpcodes.epipredict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpiPredictApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpiPredictApplication.class, args);
+    }
+
+}
