c4b85cc205e38b0746176619da9d1854032095a0
AwaPreciousNkeng <awaprecious3t@gmail.com>
2026-02-16 04:10:55 +0100

    Created default admin

--- src/main/java/com/codewithpcodes/epipredict/EpiPredictApplication.java
+++ src/main/java/com/codewithpcodes/epipredict/EpiPredictApplication.java
@@ -1,13 +1,44 @@
 package com.codewithpcodes.epipredict;
 
+import com.codewithpcodes.epipredict.auth.AuthenticationService;
+import com.codewithpcodes.epipredict.auth.RegisterRequest;
+import com.codewithpcodes.epipredict.user.Role;
+import com.codewithpcodes.epipredict.user.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class EpiPredictApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EpiPredictApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(
+            AuthenticationService service,
+            UserRepository repository,
+            UserRepository userRepository) {
+        return args -> {
+            String defaultEmail = "admin@gmail.com";
+
+            if (!userRepository.existsByEmail(defaultEmail)) {
+                var admin = new RegisterRequest(
+                        "EpiPredict",
+                        "Admin",
+                        "admin@gmail.com",
+                        "password",
+                        "",
+                        Role.ADMIN
+                );
+                System.out.println("Admin token: " + service.register(admin).getAccessToken());
+            } else {
+                System.out.println("Default ADMIN exists already, skipping creation.");
+            }
+        };
+    }
 }
